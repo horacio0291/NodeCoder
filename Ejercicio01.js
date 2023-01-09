7305da7c701746ea98982f3100baffa44e6be1e3
Horacio Santiago Ortiz <horacio0291@gmail.com>
2023-01-09 20:32:42 -0300

    Mejoro funcion getProductById

--- Ejercicio01.js
+++ Ejercicio01.js
@@ -7,14 +7,13 @@ class ProductManager {
 
   isInCart = (code) =>
     this.products.find((product) => product.code === code) ? true : false;
-  //Verificca si el codigo ya esta cargado en el array
 
   addProduct(title, description, price, thumbnail, code, stock) {
     if (this.isInCart(code)) {
       return;
       //Si la respuesta de la funcion inInCart es true quiere decir que el codigo ya esta cargado en el array
     }
-    if(title && description && price && thumbnail && code && stock){
+    if (title && description && price && thumbnail && code && stock) {
       this.products.push({
         id: this.products.length + 1,
         title,
@@ -24,8 +23,8 @@ class ProductManager {
         code,
         stock,
       });
-    } else{
-      console.log("Falta completar campos")
+    } else {
+      console.log("Complete all fields");
     }
   }
 
@@ -34,20 +33,20 @@ class ProductManager {
   }
 
   getProductById(id) {
-    if (this.products.find((product) => product.id === id)) {
-      return this.products.find((product) => product.id === id);
-    } else {
-      return "Not Found";
+    const product = this.products.find((product) => product.id === id);
+    if (!product) {
+      return "Not found";
     }
+    return product;
   }
 }
 
 const productos = new ProductManager();
 
-productos.addProduct("pizza", "comida", 100, "\ruta", 12345);
-productos.addProduct("hamburguesa", "Comida", 1000, "\ruta", 12345);
-productos.addProduct("ensalada", "Comida", 200, "\ruta", 123456);
-productos.addProduct("tostadas", "Comida", 500, "\ruta", 123456);
+productos.addProduct("pizza", "comida", 100, "\ruta", 12345, 10);
+productos.addProduct("hamburguesa", "Comida", 1000, "\ruta", 12345, 20);
+productos.addProduct("ensalada", "Comida", 200, "\ruta", 123456, 30);
+productos.addProduct("tostadas", "Comida", 500, "\ruta", 123456, 40);
 
 console.table(productos.getProducts());
-console.log(productos.getProductById(1));
+console.log(productos.getProductById(888));
